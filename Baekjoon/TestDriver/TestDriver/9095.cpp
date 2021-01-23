a777c19404739d91ccdbe5b984a1718391938afb
phatom0122 <wilko97@naver.com>
2021-01-23 13:24:40 +0900

    Create 9095.cpp

---
+++ Baekjoon/TestDriver/TestDriver/9095.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int combination(int sum, int n) {
+	
+}
+
+int main() {
+	int n;
+	int count = 0;
+	cin >> n;
+	int array[12];
+
+	
+	cout << count;
+}
